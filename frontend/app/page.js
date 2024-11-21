13d412d68c992b5407666f57d2432b629638d0e0
ernnies <ernestnnamdie@gmail.com>
2024-11-21 23:06:48 +0100

    refactored main page

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -26,7 +26,7 @@ const Home = () => {
     if (ethereum) {
       console.log('Got the ethereum Object: ', ethereum)
     } else {
-      console.log('No Wallet found. Please connec
+      console.log('No Wallet found. Please
   //Calls metamask to connect wallet on clicking Connect Wallet Button
   const connectWallet = async () => {
     try {
