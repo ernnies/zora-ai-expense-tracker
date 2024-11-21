6882218a99e8ea01ec448412baf62b02a4b7c730
ernnies <ernestnnamdie@gmail.com>
2024-11-21 22:25:06 +0100

    refactored main page

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -29,7 +29,7 @@ const Home = () => {
       console.log('No Wallet found. Please connect Wallet')
     }
 
-    const accoun
+    const ac
   //Calls metamask to connect wallet on clicking Connect Wallet Button
   const connectWallet = async () => {
     try {
